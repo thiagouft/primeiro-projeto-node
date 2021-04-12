8dfc4394930f34033f6cc401431ff1f6217c281e
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-04-12 15:05:43 -0300

    Validando a data, montando a interface appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,35 @@
-import { request, response, Router } from 'express';
+import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+
+  const pasrseDate = startOfHour(parseISO(date));
+  const findAppointmentsInSameDate = appointments.find(appointment =>
+    isEqual(pasrseDate, appointment.date),
+  );
+
+  if (findAppointmentsInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: pasrseDate,
   };
 
   appointments.push(appointment);
