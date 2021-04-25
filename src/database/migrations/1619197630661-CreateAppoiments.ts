eba5516b24d756551c4fb6690a23096c4684c988
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-04-24 23:00:06 -0300

    Configurando Repositorio, Rotas e Service com typeorm

--- src/database/migrations/1619197630661-CreateAppoiments.ts
+++ src/database/migrations/1619197630661-CreateAppoiments.ts
@@ -12,6 +12,7 @@ export default class CreateAppoiments1619197630661
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
