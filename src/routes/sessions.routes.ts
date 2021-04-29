53b85a650915bd69801dd0e4fa697b8e4c8219bd
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-04-28 22:15:45 -0300

    Gerando Token JWT

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,14 +10,14 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authenticationUser = new AuthenticationUserService();
 
-    const { user } = await authenticationUser.execute({
+    const { user, token } = await authenticationUser.execute({
       email,
       password,
     });
 
     delete user.password;
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
